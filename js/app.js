cecbcb45c9663cb08faedd37d45edb0d0b3049b7
Ryan Rayfield <ryan.rayfield@ussquash.com>
2016-01-17 16:45:59 -0500

    beautify

--- js/app.js
+++ js/app.js
@@ -1,115 +1,115 @@
 // Enemies our player must avoid
-var Enemy = function(x,y,loc) {
+var Enemy = function(x, y, loc) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x=x;
-    this.y=y;
-    this.loc=loc;
+    this.x = x;
+    this.y = y;
+    this.loc = loc;
     //console.log(loc);
     //console.log(this);
-   
-    
+
+
 };
- 
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.x+=this.loc*dt;
-    if (this.x>505) {
-        this.x=-200;            
+    this.x += this.loc * dt;
+    if (this.x > 505) {
+        this.x = -200;
     }
     checkEnemyPosition();
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x,y){
+var Player = function(x, y) {
     this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
-   // console.log(this)
+    // console.log(this)
 }
-Player.prototype.update = function(){
-   // this.loc++;
+Player.prototype.update = function() {
+    // this.loc++;
 }
 
-Player.prototype.render = function(x,y) {
-    
+Player.prototype.render = function(x, y) {
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function (key){
+Player.prototype.handleInput = function(key) {
 
-    if (key === 'up'){
-        this.y = this.y-35;
+    if (key === 'up') {
+        this.y = this.y - 35;
         // console.log(key); 
-    } else if (key === 'down'){
-        this.y = this.y+35;
+    } else if (key === 'down') {
+        this.y = this.y + 35;
         // console.log(key); 
-    } else if (key === 'left'){
-        this.x = this.x-35;
+    } else if (key === 'left') {
+        this.x = this.x - 35;
         // console.log(key); 
-    } else if (key === 'right'){
-        this.x = this.x+35;
+    } else if (key === 'right') {
+        this.x = this.x + 35;
         // console.log(key); 
-       
+
     }
-    
-    if (this.x>420) {
+
+    if (this.x > 420) {
         this.x = 420;
-    } else if (this.x<0){
+    } else if (this.x < 0) {
         this.x = 0;
-    } else if (this.y<50){
+    } else if (this.y < 50) {
         this.y = 415;
-    } else if  (this.y>415){
+    } else if (this.y > 415) {
         this.y = 415;
     }
-       
- 
+
+
 }
 
 //var collision = function (){
 //    if (allEnemies[1].x=player.x)
 //    console.log('hit');
 //    }
-    
 
-var allEnemies =  [];
-var dracula = new Enemy(50,75,300);
+
+var allEnemies = [];
+var dracula = new Enemy(50, 75, 300);
 allEnemies.push(dracula);
-var devil = new Enemy(-100,120,100);
+var devil = new Enemy(-100, 120, 100);
 allEnemies.push(devil);
-var player= new Player(205,415);
+var player = new Player(205, 415);
 
-var checkEnemyPosition = function(i){
-for (i=0; i < allEnemies.length; i++){
-    if ((allEnemies[i].x >= (-50+player.x) && allEnemies[i].x <= (50+player.x)) && (allEnemies[i].y >= (-50+player.y) && allEnemies[i].y <= (50+player.y))){
-        resetPlayer();
-    } //else {
-      //  console.log('no');
-   // }
-   // console.log(allEnemies[i].x);
-    //console.log(player.x + ', '+ player.y);
+var checkEnemyPosition = function(i) {
+    for (i = 0; i < allEnemies.length; i++) {
+        if ((allEnemies[i].x >= (-50 + player.x) && allEnemies[i].x <= (50 + player.x)) && (allEnemies[i].y >= (-50 + player.y) && allEnemies[i].y <= (50 + player.y))) {
+            resetPlayer();
+        } //else {
+        //  console.log('no');
+        // }
+        // console.log(allEnemies[i].x);
+        //console.log(player.x + ', '+ player.y);
+    }
 }
-}
-var resetPlayer = function (){
-    player.x=205;
-    player.y=415;
+var resetPlayer = function() {
+    player.x = 205;
+    player.y = 415;
 }
 
 
